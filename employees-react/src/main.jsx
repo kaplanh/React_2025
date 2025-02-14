37e1b46bbf3a5fae48acec5b880aaad93ad7b0c7
kplnhsyn <kplnhsyn.49@gmail.com>
2025-02-14 02:36:51 +0100

    modal added

--- employees-react/src/main.jsx
+++ employees-react/src/main.jsx
@@ -12,17 +12,25 @@ function App() {
             phone: "(204) 619-5731",
         },
     ]);
+    const [isAddModalOpen, setisAddModalOpen] = useState(false);
+
     return (
         <div className="container">
             <div className="table-wrapper">
-                <Header />
-                <EmployeeList employees={employees} />
+                <Header setisAddModalOpen={setisAddModalOpen} />
+                <EmployeeList
+                    employees={employees}
+                    setisAddModalOpen={setisAddModalOpen}
+                />
+                {isAddModalOpen ? (
+                    <AddEmployeeModal setisAddModalOpen={setisAddModalOpen} />
+                ) : null}
             </div>
         </div>
     );
 }
 
-function Header() {
+function Header({ setisAddModalOpen }) {
     return (
         <div className="table-title">
             <div className="row">
@@ -32,25 +40,123 @@ function Header() {
                     </h2>
                 </div>
                 <div className="col-sm-6">
-                    <a
-                        href="#addEmployeeModal"
+                    <button
+                        onClick={() => setisAddModalOpen(true)}
                         className="btn btn-success"
-                        data-toggle="modal"
                     >
                         <i className="material-icons">&#xE147;</i>{" "}
                         <span>Add New Employee</span>
-                    </a>
-                    <a
-                        href="#deleteEmployeeModal"
-                        className="btn btn-danger"
-                        data-toggle="modal"
-                    >
+                    </button>
+                    <button className="btn btn-danger">
                         <i className="material-icons">&#xE15C;</i>{" "}
                         <span>Delete</span>
-                    </a>
+                    </button>
                 </div>
             </div>
         </div>
+    );
+}
+
+function AddEmployeeModal({ setisAddModalOpen }) {
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        address: "",
+        phone: "",
+    });
+    function handleChange(e) {
+        const { name, value } = e.target;
+        setFormData((prevState) => ({ ...prevState, [name]: value }));
+        // setFormData({ ...formData, [e.target.name]: e.target.value });
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        // console.log("formData", formData);
+    }
+    return (
+        <>
+            <div id="addEmployeeModal" className="modal fade show">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <form onSubmit={handleSubmit}>
+                            <div className="modal-header">
+                                <h4 className="modal-title">Add Employee</h4>
+                                <button
+                                    onClick={() => setisAddModalOpen(false)}
+                                    type="button"
+                                    className="close"
+                                    data-dismiss="modal"
+                                    aria-hidden="true"
+                                >
+                                    &times;
+                                </button>
+                            </div>
+                            <div className="modal-body">
+                                <div className="form-group">
+                                    <label>Name</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        required
+                                        name="name"
+                                        value={formData.name}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label>Email</label>
+                                    <input
+                                        type="email"
+                                        className="form-control"
+                                        required
+                                        name="email"
+                                        value={formData.email}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label>Address</label>
+                                    <textarea
+                                        className="form-control"
+                                        required
+                                        name="address"
+                                        value={formData.address}
+                                        onChange={handleChange}
+                                    ></textarea>
+                                </div>
+                                <div className="form-group">
+                                    <label>Phone</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        required
+                                        name="phone"
+                                        value={formData.phone}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                            </div>
+                            <div className="modal-footer">
+                                <button
+                                    type="button"
+                                    className="btn btn-default"
+                                >
+                                    Cancel
+                                </button>
+                                <button
+                                    type="submit"
+                                    className="btn btn-success"
+                                >
+                                    Add
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div className="modal-backgrop fade show"></div>
+        </>
     );
 }
 function EmployeeList({ employees }) {
